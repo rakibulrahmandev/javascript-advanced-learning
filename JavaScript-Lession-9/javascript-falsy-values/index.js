5b821eac25b871e3aca619386f8715b6a4912aba
devrakibulrahman <devrakibulrahman@gmail.com>
2025-03-20 13:34:46 +0600

    javascript truthy and falsy value #javascript_deep_learning

---
+++ JavaScript-Lession-9/javascript-falsy-values/index.js
@@ -0,0 +1,9 @@
+// ? javascript have some truthy and falsy value -------------------------------->
+
+// 1. false - Boolean false
+// 2. 0 - The number zero
+// 3. -0 - Negative zero
+// 4. "" (empty string) - A string with no characters
+// 5. null - Absence of value
+// 6. undefined - Variable declared but not assigned a value
+// 7. NaN - Not a number
---
+++ JavaScript-Lession-9/javascript-truthy-values/index.js
@@ -0,0 +1,8 @@
+// ? javascript have some truthy and falsy value -------------------------------->
+
+// 1. true
+// 2. Any non-zero number (e.g., 1, -5, 3.14)
+// 3. Any non-empty string (e.g., "hello", "false")
+// 4. Any object (e.g., {}, [])
+// 5. Any function (e.g., function() {})
+// 6. Special values like Infinity and -Infinity
