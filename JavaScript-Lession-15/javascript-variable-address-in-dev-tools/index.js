473eb1198e596203c633cc2d690bb39320146c72
devrakibulrahman <devrakibulrahman@gmail.com>
2025-03-20 13:41:32 +0600

    javascript variables in dev-tools #javascript_deep_learning

---
+++ JavaScript-Lession-15/javascript-variable-address-in-dev-tools/index.js
@@ -0,0 +1,23 @@
+// ? declare the string variables --------------------------------------------------------------------->
+
+const fname = 'Rakibul';
+const lname = 'Rahman';
+const fullname = fname + ' ' + lname;
+
+// ? declare the number variables --------------------------------------------------------------------->
+
+const myAge = 18;
+const myBirthYear = 2006;
+
+// ? declare the boolean variables -------------------------------------------------------------------->
+
+const hasJob = false;
+const hasPassion = true;
+
+// ? declare the false value variables ---------------------------------------------------------------->
+
+const nullObject = null;
+const undefinedValue = undefined;
+
+// ! remember that, this variable are primitive data type so we no need to get variable address. but sometimes we need to get primitive data type variable address.
+// ! so you think why we need to get variable address? good question, we need this variable address when we work with non-primitive data types like array, object, function etc.
