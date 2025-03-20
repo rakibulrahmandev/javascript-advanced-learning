a02ceb67e7493ed613d3950790a477fa251fb0e7
devrakibulrahman <devrakibulrahman@gmail.com>
2025-03-20 13:25:59 +0600

    javascript first code hello world #javascript_deep_learning

---
+++ JavaScript-Lession-1/hello-world/index.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+console.log("My Name Rakibul Rahman");
+console.log("My Dream Is Software Engineering");
+console.log("I Am 18 Years Old");
+console.log("My SSC GPA Is 3.98 Out Of 5.00");
+console.log("I Want To Go And Study In Korean Universities");
+console.log(50 + 50);
+console.log("50" + 50);
