b8f7212bea07871e245163609e99e73a76882a26
devrakibulrahman <devrakibulrahman@gmail.com>
2025-04-07 19:46:22 +0600

    javascript combining assignment operators. #javascript_deep_learning

---
+++ JavaScript-Lession-20/javascript-combining-operators/index.js
@@ -0,0 +1,123 @@
+//? javascript combining assignment operators ----------------------------------------------------------------------------------------------------------->
+
+// Combining assignment operators in JavaScript (and most other programming languages) allow you to perform an operation and assign the result to a variable in a shorthand way.
+
+//! javascript add and assigns operators (+=) ----------------------------------------------------------->
+
+//* ex-1
+let num1 = 5;
+
+// old and manual version:
+num1 = num1 + 1;
+
+// let's check the output:
+console.log(num1); // output: 6
+
+//* ex-2
+let num2 = 10;
+
+// using combining operators:
+num2 += 2;
+
+// let's check the output:
+console.log(num2); // output: 12
+
+//! javascript subtracts and assigns operators (-=) ----------------------------------------------------->
+
+//* ex-1
+let num3 = 20;
+
+// old and manual version:
+num3 = num3 - 5;
+
+// let's check the output:
+console.log(num3); // output: 15
+
+//* ex-2
+let num4 = 50;
+
+// using combining operators:
+num4 -= 10;
+
+// let's check the output:
+console.log(num4); // output: 40
+
+//! javascript multiplies and assigns operators (*=) ---------------------------------------------------->
+
+//* ex-3
+let num5 = 10;
+
+// old and manual version:
+num5 = num5 * 5;
+
+// let's check the output: 
+console.log(num5); // output: 50
+
+//* ex-4
+let num6 = 20;
+
+// using assignment operators:
+num6 *= 2;
+
+// let's check the output:
+console.log(num6); // output: 40
+
+//! javascript divides and assigns operators (/=) ------------------------------------------------------->
+
+//* ex-5
+let num7 = 10;
+
+// old and manual version:
+num7 = num7 / 2;
+
+// let's check the output:
+console.log(num7); // output: 5
+
+//* ex-6
+let num8 = 5;
+
+// using assignment operators:
+num8 /= 2;
+
+// let't check the output:
+console.log(num8); // output: 2.5
+
+//! javascript modules and assigns (remainder, %=) ------------------------------------------------------>
+
+//* ex-7
+let num9 = 40;
+
+// old and manual version:
+num9 = num9 % 2;
+
+// let's check the output:
+console.log(num9); // output: 0
+
+//* ex-8
+let num10 = 50;
+
+// using assignment operators:
+num10 %= 3;
+
+// let's check the output:
+console.log(num10); // output: 2
+
+//! javascript exponentiation and assigns (**=) --------------------------------------------------------->
+
+//* ex-9
+let num11 = 10;
+
+// old and manual version:
+num11 = num11 ** 2;
+
+// let's check the output:
+console.log(num11); // output: 100
+
+//* ex-10
+let num12 = 20;
+
+// using assignment operators:
+num12 **= 3;
+
+// let's check the output:
+console.log(num12); // output: 8000
