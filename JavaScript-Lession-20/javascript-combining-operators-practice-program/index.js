9b0c2f64740640f87076f93b5ce2b3768f3c0055
devrakibulrahman <devrakibulrahman@gmail.com>
2025-04-07 20:19:47 +0600

    javascript combining assignment operators practice program done. #javascript_deep_learning

---
+++ JavaScript-Lession-20/javascript-combining-operators-practice-program/index.js
@@ -0,0 +1,40 @@
+//? javascript combining operators practice program ------------------------------------------------------------------------------------------------>
+
+//! Quiz-1 ----------------------------------------------------------------------->
+let num1 = 4;
+
+// update value with combining operators:
+num1 += 3;
+
+// let's check the output:
+console.log(num1); // output: 7
+
+//! Quiz-2 ----------------------------------------------------------------------->
+let num2 = 10;
+
+// update value with combining operators:
+num2 *= 2;
+num2 -= 5;
+
+// let's check the output:
+console.log(num2); // output: 15
+
+//! Quiz-3 ----------------------------------------------------------------------->
+let num3 = 9;
+
+// update value with combining operators:
+num3 /= 3;
+num3 += 2;
+num3 %= 4;
+
+// let's check the output:
+console.log(num3); // output: 1
+
+//! Quiz-4 ----------------------------------------------------------------------->
+let num4 = 2;
+
+// update value with combining operators:
+num4 **= 3;
+
+// let's check the output:
+console.log(num4); // output: 8
