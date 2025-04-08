66f12c9765fdf4c5f89414136ada0e4f1d7fd383
devrakibulrahman <devrakibulrahman@gmail.com>
2025-04-08 21:23:53 +0600

    javascript decrement operators. #javascript_deep_learning

---
+++ JavaScript-Lession-20/javascript-decrement-operators/index.js
@@ -0,0 +1,41 @@
+//? javascript decrement operators ------------------------------------------------------------------------------------------------------------------>
+
+//! javascript decrement additional operators (--) ------------------------------------------------------------------------------->
+
+//* ex-1
+let num1 = 10;
+
+// using decrement operators:
+num1--
+
+// same as:
+num1 = num1 - 1;
+
+// let's check the output:
+console.log(num1); // output: 8
+
+//? increment and decrement operators tow types: Pre vs Post
+
+//* ex-1. Post-Increment (x++):
+let num2 = 5;
+
+// using increment additional after the "num1":
+num2--
+
+// let's check the output:
+console.log(num2); // output: 6
+
+//! note:
+// Post-Increment (x++). Returns the value first, then increases it.
+
+//* ex-2. Pre-Increment (++y):
+let num3 = 5;
+
+// using increment additional before the "num2":
+--num3;
+
+// let's check the output:
+console.log(num3); // output: 6
+
+//! note:
+// Per-Increment (++y). Increases the value first, then returns it.
