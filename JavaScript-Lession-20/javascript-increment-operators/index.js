099274c750a54865b7e7a58881f57d9443f38850
devrakibulrahman <devrakibulrahman@gmail.com>
2025-04-08 21:14:36 +0600

    javascript increment operators. #javascript_deep_learning

---
+++ JavaScript-Lession-20/javascript-increment-operators/index.js
@@ -0,0 +1,41 @@
+//? javascript increment operators ------------------------------------------------------------------------------------------------------------------>
+
+//! javascript increment additional operators (++) ------------------------------------------------------------------------------->
+
+//* ex-1
+let num = 5;
+
+// using increment additional operators:
+num++;
+
+// Same as:
+num = num + 1;
+
+// let's check the output:
+console.log(num); // output: 7
+
+//? increment and decrement operators tow types: Pre vs Post
+
+//* ex-1. Post-Increment (x++):
+let num1 = 5;
+
+// using increment additional after the "num1":
+num1++
+
+// let's check the output:
+console.log(num1); // output: 6
+
+//! note:
+// Post-Increment (x++). Returns the value first, then increases it.
+
+//* ex-2. Pre-Increment (++y):
+let num2 = 5;
+
+// using increment additional before the "num2":
+++num2;
+
+// let's check the output:
+console.log(num2); // output: 6
+
+//! note:
+// Per-Increment (++y). Increases the value first, then returns it.
